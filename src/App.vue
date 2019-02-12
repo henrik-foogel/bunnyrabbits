a5db58b1d172b2307506b2664b89399b408c1126
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-12 15:49:21 +0100

    fistPage

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-   
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  background: #231F42;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <img src="../assets/logo.svg" alt="">
+    <h1> Were it´s @!</h1>
+    <h2>Ticketing made easy..</h2>
   </div>
 </template>
 
@@ -11,3 +14,25 @@ export default {
   }
 }
 </script>
+<style>
+@import url("https://fonts.googleapis.com/css?family=Sansita:400,800i");
+body {
+  background-color:rgb(45, 3, 80);
+  text-align: center;
+}
+img{
+  margin-top: 50%;
+}
+h1{
+  font-family: 'Sansita', sans-serif;
+  font-size: 2rem;
+  margin: 0%;
+  padding: 0%;
+  color: #F56B9A;
+}
+h2 {
+  font-size: .7rem;
+  font-style: italic;
+  color: whitesmoke;
+}
+</style>
