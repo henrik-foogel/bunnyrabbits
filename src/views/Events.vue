b04bb17081bd3298ae7402972369735f2516da59
Henrik Foogel <henrik.foogel@medieinstitutet.se>
2019-02-12 16:02:18 +0100

    grid css ticket & events

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,5 +1,67 @@
 <template>
   <div class="events">
-    <h1>EVENTS</h1>
+    <section class="event-title">Events</section>
+    <section class="event-search"><input type="text"></section>
+    <section class="event-list"></section>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'events'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Sansita+One');
+
+  .events {
+    max-width: 17rem;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1;
+    grid-template-rows: 60px 60px 120px;
+    grid-template-areas: "event-title"
+    "event-search"
+    "event-list"
+  }
+
+  .event-title {
+    grid-area: event-title;
+    font-family: 'Sansita One';
+    font-style: normal;
+    font-weight: normal;
+    line-height: normal;
+    font-size: 32px;
+    text-align: center;
+    color: #F56B9A;
+    margin-top: 1rem;
+  }
+  .event-search {
+    grid-area: event-search;
+    background: firebrick;
+  }
+  .event-list {
+    grid-area: event-list;
+    background: steelblue;
+  }
+
+  /*.event-title {
+    position: absolute;
+    width: 311px;
+    height: 45px;
+    left: 32px;
+    top: 34px;
+    font-family: 'Sansita One';
+    font-style: normal;
+    font-weight: normal;
+    line-height: normal;
+    font-size: 32px;
+    text-align: center;
+    color: #F56B9A;
+  }
+
+  .event-search {
+    position: absolute;
+  }*/
+</style>
--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="tickets">
-    <h1>TICKETS</h1>
-  </div>
+  <article class="tickets">
+    <section class="what"></section>
+    <section class="where"></section>
+    <section class="when"></section>
+    <section class="from"></section>
+    <section class="to"></section>
+    <section class="info"></section>
+    <section class="code"></section>
+  </article>
 </template>
+
+<style scoped>
+.tickets {
+  max-width: 17rem;
+  width: 100%;
+  background: green;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 60px 120px 60px 60px 120px;
+  grid-template-areas: "what what what"
+  "where where where"
+  "when from info"
+  "code code code"
+}
+
+.what {
+  grid-area: what;
+  background: #4f4f4f;
+}
+.where {
+  grid-area: where;
+  background: #577657;
+}
+.when {
+  grid-area: when;
+  background: #112222;
+}
+.from {
+  grid-area: from;
+  background: #989898;
+}
+.to {
+  grid-area: to;
+  background: #1122dd;
+}
+.info {
+  grid-area: info;
+  background: #12ab11;
+}
+.code {
+  grid-area: code;
+  background: #577657;
+}
+</style>
