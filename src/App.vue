be41547379a677740eb15e40d164a5e8525bdac9
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-13 12:44:05 +0100

    beforeMerge

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-  body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0;
-  background: #231F42;
-}
 
 </style>
