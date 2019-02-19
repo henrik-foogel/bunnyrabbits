4eceea387d7e90bf6faa186aecba35b526a8215f
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-19 22:05:44 +0100

    Sassing to hell out of tikket

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,12 +1,12 @@
 <template>
 <article class="ticket">
-    <section class="what">{{ticket.event.name}}</section>
-    <section class="where">where</section>
-    <section class="when">when</section>
-    <section class="from">from</section>
-    <section class="to">to</section>
-    <section class="info">info</section>
-    <section class="code">code</section>
+    <section class="what"><h3 class="rubrik">Evenemang: <br> {{ticket.event.name}}</h3></section>
+    <section class="where"><h3 class="rubrik">Vart: <br> {{ticket.event.where}}</h3></section>
+    <section class="date"><h3 class="rubrik">Datum: <br> {{ticket.event.date.month}} / {{ticket.event.date.day}}</h3></section>
+    <section class="from"><h3 class="rubrik">Börjar: <br> {{ticket.event.when.from}}</h3></section>
+    <section class="to"><h3 class="rubrik">Slutar: <br> {{ticket.event.when.to}}</h3></section>
+    <section class="price"><h3 class="rubrik">Pris: <br>{{ticket.event.price}}:-</h3></section>
+    <section class="code"><h3 class="rubrik">Vertifiering code:</h3><h3 id="barcode"> <br> {{ticket.code}}</h3></section>
   </article>
 </template>
 
@@ -19,47 +19,64 @@ export default {
 }
 </script>
 
-<style scoped>
-.tickets {
-  color: white;
-  max-width: 19rem;
-  width: 100%;
-  background: green;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 60px 120px 60px 60px 120px;
-  grid-template-areas: "what what what"
-  "where where where"
-  "when from info"
-  "code code code"
+<style lang="scss" scoped>
+@font-face{
+  font-family: 'barCode';
+  src: url(../assets/barcode.ttf);
 }
+  .ticket {
+      color: #000;
+      max-width: 20rem;
+      width: 100%;
+      background: green;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-rows: 60px 60px 60px 60px 60px 120px;
+      grid-template-areas:
+      "what what what"
+      "where where where"
+      "date date date"
+      "from . to"
+      "price price price"
+      "code code code";
 
-.what {
-  grid-area: what;
-  background: #4f4f4f;
-}
-.where {
-  grid-area: where;
-  background: #577657;
-}
-.when {
-  grid-area: when;
-  background: #112222;
-}
-.from {
-  grid-area: from;
-  background: #989898;
-}
-.to {
-  grid-area: to;
-  background: #1122dd;
-}
-.info {
-  grid-area: info;
-  background: #12ab11;
-}
-.code {
-  grid-area: code;
-  background: #577657;
+      h3 {
+        margin: .2rem;
+      }
+      .what {
+      grid-area: what;
+      background: #9c0808;
+      }
+      .where {
+      grid-area: where;
+      background: #577657;
+      }
+      .date {
+        grid-area: date;
+        background: hotpink;
+      }
+      .from {
+      grid-area: from;
+      background: #989898;
+      }
+      .to {
+      grid-area: to;
+      background: #1122dd;
+      }
+      .price {
+        grid-area: price;
+        background: saddlebrown;
+      }
+      .code {
+      grid-area: code;
+      background: #9c0808;
+
+      #barcode {
+      font-family: barCode;
+      font-size: 1rem;
+      line-height: 1rem;
+      padding-top: 1.2rem;
+      }  
+  }
 }
 </style>
