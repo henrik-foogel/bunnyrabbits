e191f4e3f22c8dc4eb1c72b7d4f8d9de81b76cc7
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-20 20:00:04 +0100

    more sass

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -28,75 +28,76 @@ export default {
  @import url('https://fonts.googleapis.com/css?family=Fira+Sans');
 
  /* Kanske onödigt att ha grid när ändå allt bara ska ligga påvaranndra, men övning ger färdighet, så vi kör på de ändå!*/
-  body {
-
-     background-color:$orange;
- }
-
- #staff {
-    max-width: 20rem;
-    width: 100%;
-    height: 30rem;
-    display: grid;
-    grid-template-rows: 180px 180px 90px 90px;
-    grid-template-areas:
-     "logo"
-    "img"
-    "input"
-    "btn"
-  }
+body {
+    background-color:$orange;
+    overflow: hidden;
+ 
+        #staff {
+            max-width: 20rem;
+            width: 100%;
+            height: 30rem;
+            display: grid;
+            grid-template-rows: 180px 180px 90px 90px;
+            grid-template-areas:
+            "logo"
+            "img"
+            "input"
+            "btn"
+        }
 
     .logo {
-  grid-area: logo;
-  filter: grayscale(100%);
-}  
-.logo p {
-    text-transform: uppercase;
-    font-family: 'Fira Sans', sans-serif;
-    font-size: 22px;
-    text-align: center;
-    letter-spacing: 0.3em;
-    
-}
-#small {
-    width: 8rem;
-}
- .img {
-    grid-area: img;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}  
-.pass {
-    width: 10rem;
-    border-radius: 50%;
-}
- .input {
-  grid-area: input;
-  border: 2px solid #231F42;
-} 
-.input input {
-    width: 99%;
-    height: 100%;
-    font-family: 'Fira Sans', sans-serif;
-    font-weight: bold;
-    font-size: 30px;
-    letter-spacing: .2em;
-    color: #000;
-    text-align: center;
-}
+        grid-area: logo;
+        filter: grayscale(100%);
 
-  .btn {
-  grid-area: btn;
-} 
-.btn button {
-    background-color: #231F42;
-    width: 100%;
-    height: 100%;
-    font-family: 'Fira Sans', sans-serif;
-    font-weight: bold;
-    font-size: 26px;
-    letter-spacing: .2em;
-    color: #fff;
+         p {
+            text-transform: uppercase;
+            font-family: 'Fira Sans', sans-serif;
+            font-size: 22px;
+            text-align: center;
+            letter-spacing: 0.3em;
+        }  
+        #small {
+            width: 8rem;
+        }
+        }
+    .img {
+        grid-area: img;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        }  
+    .pass {
+        width: 10rem;
+        border-radius: 50%;
+        }
+    .input {
+        grid-area: input;
+        border: 2px solid #231F42;
+        } 
+        input {
+            width: 99%;
+            height: 100%;
+            font-family: 'Fira Sans', sans-serif;
+            font-weight: bold;
+            font-size: 30px;
+            letter-spacing: .2em;
+            color: #000;
+            text-align: center;
+        }
+
+    .btn  {
+        grid-area: btn;
+
+        button {
+        background-color: #231F42;
+        width: 100%;
+        height: 100%;
+        font-family: 'Fira Sans', sans-serif;
+        font-weight: bold;
+        font-size: 26px;
+        letter-spacing: .2em;
+        color: #fff;
+    } 
+  }
 }
 </style>
