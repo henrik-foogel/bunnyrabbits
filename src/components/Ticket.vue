b5bd6cc642fa041bd425743bab2121847d1bff58
henrik-foogel <henrik.foogel@medieinstitutet.se>
2019-02-20 15:53:24 +0100

    Made ticket css nicer again

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,10 +1,11 @@
 <template>
 <article class="ticket">
+  
     <section class="what">{{ticket.event.name}}</section>
-    <section class="where">{{ticket.event.where}}</section>
-    <section class="when">{{ticket.event.date.day}}  {{ticket.event.date.month}}</section>
-    <section class="from">{{ticket.event.when.from}}</section>
-    <section class="to">{{ticket.event.when.to}}</section>
+    <section class="where"><h3><span>VENUE: </span><br> {{ticket.event.where}}</h3></section>
+    <section class="when"><h3><span>DATE: </span><br> {{ticket.event.date.day}}  {{ticket.event.date.month}}</h3></section>
+    <section class="from"><h3><span>FROM: </span><br> {{ticket.event.when.from}}</h3></section>
+    <section class="to"><h3><span>TO: </span><br> {{ticket.event.when.to}}</h3></section>
     <section class="code">{{ticket.code}}</section>
   </article>
 </template>
@@ -29,7 +30,7 @@ export default {
   border-radius: 5px;
   margin-bottom: 1rem;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 60px 40px 60px 100px;
+  grid-template-rows: 60px 75px 70px 100px;
   grid-template-areas: "what what what"
   "where where where"
   "when from to"
@@ -50,7 +51,6 @@ export default {
   font-size: 1.5rem;
   font-weight: 700;
   display: flex;
-  align-items: center;
   justify-content: center;
 }
 .when {
@@ -58,7 +58,6 @@ export default {
   background: #ddd;
   border: 1px dotted black;
   display: flex;
-  align-items: center;
   justify-content: center;
   font-size: 1.3rem;
   font-weight: 800;
@@ -68,7 +67,6 @@ export default {
   background: #ddd;
   border: 1px dotted black;
   display: flex;
-  align-items: center;
   justify-content: center;
   font-size: 1.3rem;
   font-weight: 800;
@@ -78,7 +76,6 @@ export default {
   background: #ddd;
   border: 1px dotted black;
   display: flex;
-  align-items: center;
   justify-content: center;
   font-size: 1.3rem;
   font-weight: 800;
@@ -92,5 +89,14 @@ export default {
   justify-content: center;
   border-bottom-right-radius: 5px;
   border-bottom-left-radius: 5px;
+}
+h3 {
+  margin:0;
+}
+span {
+  font-size: 0.6rem;
+  font-weight: 800;
+  color: #555;
+  padding: 0;
 }
 </style>
